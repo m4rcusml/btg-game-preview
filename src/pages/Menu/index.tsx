8a2a6bc5d36b18ae4046dfa32fb5cc35a63625bc
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-02-19 14:07:46 -0300

    add grid layout to menu css

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,17 +1,19 @@
+import { useNavigate } from 'react-router';
 import { MenuButton } from '../../components/buttons/Menu';
 import './style.css';
 
 export function Menu() {
+  const navigate = useNavigate();
+
   return (
     <main id="menu">
       <div className="buttons">
         <MenuButton label='Play' />
 
-        <div>
-          <MenuButton label='Opt' />
-          <MenuButton label='Lang' />
-          <MenuButton label='Exit' />
-        </div>
+        <MenuButton label='Opt' onClick={() => navigate('options')} />
+
+        <MenuButton label='Lang' />
+        <MenuButton label='Exit' />
       </div>
     </main>
   )
--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router';
+import './style.css';
+
 export function Options() {
+  const navigate = useNavigate();
+
   return (
-    <main></main>
+    <main id='optContainer'>
+      <div id="optBox">
+        <button onClick={() => navigate(-1)}>Voltar para o menu</button>
+
+        <div>
+          <label htmlFor="volume">Volume:</label>
+          <input type="range" id='volume' min={0} max={100} />
+        </div>
+      </div>
+    </main>
   )
 }
