c2d3b23b4a800d135d376c8e07dadcbdc01181de
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-02-19 17:46:18 -0300

    feat: use localStorage to save volume

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -1,8 +1,16 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import './style.css';
 
 export function Options() {
   const navigate = useNavigate();
+  const [volume, setVolume] = useState(() => {
+    return parseInt(localStorage.getItem('volume')!, 10) ?? 100;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('volume', volume.toString());
+  }, [volume]);
 
   return (
     <main id='optContainer'>
@@ -10,10 +18,19 @@ export function Options() {
         <button onClick={() => navigate(-1)}>Voltar para o menu</button>
 
         <div>
-          <label htmlFor="volume">Volume:</label>
-          <input type="range" id='volume' min={0} max={100} />
+          <div className='field'>
+            <label htmlFor="volume">Volume:</label>
+            <input
+              type="range"
+              id='volume'
+              min={0}
+              max={100}
+              value={volume}
+              onChange={e => setVolume(parseInt(e.target.value, 10))}
+            />
+          </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
