755cc3035ba6e207a8a382136a023aff4d939e1d
José Lima <jose.lima@sou.inteli.edu.br>
2025-02-20 18:37:29 -0300

    feat: updated menu elements & changes

--- src/components/buttons/Menu/index.tsx
+++ src/components/buttons/Menu/index.tsx
@@ -3,12 +3,13 @@ import './style.css';
 type Props = {
   label?: string;
   icon?: string;
+  squared?: boolean;
   onClick?(): void;
 }
 
-export function MenuButton({ label, icon, onClick }: Props) {
+export function MenuButton({ label, icon, squared, onClick }: Props) {
   return (
-    <button className='menuBtn' onClick={onClick}>
+    <button className='menuBtn' style={squared ? {aspectRatio: 1} : undefined} onClick={onClick}>
       {icon && <img src={icon} alt='icon' />}
       {label && <p>{label}</p>}
     </button>
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { MenuButton } from '../../components/buttons/Menu';
 import logo from '../../assets/logo/logoGame.png';
@@ -5,18 +6,24 @@ import './style.css';
 
 export function Menu() {
   const navigate = useNavigate();
+  const [isPortuguese, setIsPortuguese] = useState(true);
+
+  function handleChangeIcon() {
+    setIsPortuguese(!isPortuguese);
+  }
 
   return (
     <main id="menu">
       <img className="logo" src={logo} alt="Logo" />
 
       <div className="buttons">
-        <MenuButton label='Play' />
-
-        <MenuButton label='Opt' onClick={() => navigate('options')} />
-
-        <MenuButton label='Lang' />
-        <MenuButton label='Exit' />
+        <MenuButton icon='../src/assets/icons/playIcon.png'/>
+        
+        <div className='bottom'>
+          <MenuButton icon='../src/assets/icons/optionsIcon.png' squared onClick={() => navigate('options')} />
+          <MenuButton icon={isPortuguese ? '../src/assets/icons/portIcon.png' : '../src/assets/icons/engIcon.png'} squared onClick={handleChangeIcon} />
+          <MenuButton icon='../src/assets/icons/leaveIcon.png' squared onClick={() => window.close()} />
+        </div>
       </div>
     </main>
   )
