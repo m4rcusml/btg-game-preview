e0d727078088b52c7d72ddee3077030b213f4071
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-02-20 13:32:12 -0300

    update menuBtn to support icons

--- src/components/buttons/Menu/index.tsx
+++ src/components/buttons/Menu/index.tsx
@@ -2,13 +2,15 @@ import './style.css';
 
 type Props = {
   label: string;
+  icon?: string;
   onClick?(): void;
 }
 
-export function MenuButton({ label, onClick }: Props) {
+export function MenuButton({ label, icon, onClick }: Props) {
   return (
     <button className='menuBtn' onClick={onClick}>
-      <p>{label}</p>
+      {icon && <img src={icon} alt='icon' />}
+      {label && <p>{label}</p>}
     </button>
   )
 }
